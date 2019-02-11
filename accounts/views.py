23b026990e536406c14fc93437562a4d7ddf6a65
Joe Stokes <joestokes1994@gmail.com>
2019-02-11 14:18:57 +0000

    fixed form not prefilling with form data when user wants to update profile

--- accounts/views.py
+++ accounts/views.py
@@ -54,7 +54,8 @@ def profile(request):
     
 @login_required
 def edit_profile(request):
-    profile_form = ProfileForm(request.POST, request.FILES, instance=request.user.profile)
+    user = Profile.objects.get(id=1)
+    profile_form = ProfileForm(request.POST or None, instance=request.user.profile)
     return render(request, 'edit_profile.html', {
         'profile_form': profile_form
     })
